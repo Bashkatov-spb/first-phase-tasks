a986c75e9074adc75674a868c587dca6903a3865
PenRomain <romandreevich@icloud.com>
2023-01-03 15:38:47 +0300

    task1

--- task1.js
+++ task1.js
@@ -43,8 +43,10 @@ const data = [
 
 // Необходимо вернуть массив, содержащий самого старшего человека в списке. Если несколько людей имеют одинаковый наибольший возраст, то нужно вернуть массив, содержащий их всех.
 
-const getMostSenior = () => {
-  // твой код тут
+const getMostSenior = (data) => {
+  let arr = [];
+  arr = data.slice().sort((a, b) => b.age - a.age);
+  return arr.filter((el) => arr[0].age === el.age);
 };
 
 const result = getMostSenior(data);
