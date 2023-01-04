2b770394c283c507e5a409716de2de4fc4d0b40b
PenRomain <romandreevich@icloud.com>
2023-01-04 21:13:08 +0300

    workers

--- workers.js
+++ workers.js
@@ -17,7 +17,18 @@ class Work {
 
   paySalary(worker) {
     return (worker.money +=
-      this.salary + Math.ceil((this.salary / 100) * this.worker.stage * 2));
+      this.salary + Math.ceil((this.salary / 100) * this.worker.stage * 3));
+  }
+
+  factoryAccident(worker) {
+    console.log(worker.paidHoliday());
+    return this.paySalary(worker);
+  }
+}
+
+class Shop {
+  constructor(firstNeeds) {
+    this.firstNeeds = firstNeeds;
   }
 }
 
@@ -32,14 +43,103 @@ class Worker {
 
   sayHi() {
     return this.items.length === 0
-      ? `Привет меня зовут ${this.name}, работаю уже ${this.stage} лет и у меня ${this.money} рублей.`
-      : `Привет меня зовут ${this.name}, работаю уже ${this.stage} лет и у меня ${this.money} рублей и есть: ${this.items}`;
+      ? `Привет меня зовут ${this.name}, мне ${this.age}, работаю уже ${this.stage} лет и у меня ${this.money} рублей.`
+      : `Привет меня зовут ${this.name}, мне ${this.age}, работаю уже ${this.stage} лет и у меня ${this.money} рублей и есть: ${this.items}`;
+  }
+
+  eatFood() {
+    return (this.money -= 200 * 3);
+  }
+
+  spendTime() {
+    return (this.age += 1);
+  }
+
+  growStage() {
+    return (this.stage += 1);
+  }
+
+  paidHoliday() {
+    return `Ушел в оплачиваемый отпуск на 14 дней.`;
+  }
+
+  want_to_buy(shop, wish) {
+    shop.firstNeeds.forEach((el) => {
+      if (el.item === wish && this.money >= el.cost) {
+        this.items.push(el.item);
+        el.pisces -= 1;
+        this.money -= el.cost;
+      }
+    });
+  }
+
+  drink_weekend(wish) {
+    this.items = this.items.filter((el) => el !== wish);
+  }
+
+  smoke(wish) {
+    this.items = this.items.filter((el) => el !== wish);
+    return Math.floor(Math.random() * 10_000);
   }
 }
 
 const konstantin = new Worker('Константин', 23, 5);
 const rabota = new Work(konstantin, 30_000);
+const at_krasnoeBeloe = new Shop([
+  {
+    item: 'vodka',
+    cost: 230,
+    pisces: 100,
+  },
+  {
+    item: 'beer',
+    cost: 50,
+    pisces: 1_000,
+  },
+  {
+    item: 'cigarets',
+    cost: 100,
+    pisces: 10_000,
+  },
+  {
+    item: 'redbull',
+    cost: 180,
+    pisces: 1_000,
+  },
+]);
 
-rabota.paySalary(konstantin);
+for (let i = 0; i < 25_000; i += 1) {
+  if (i % 2 === 0) {
+    konstantin.want_to_buy(at_krasnoeBeloe, 'cigarets');
+  }
+  if ((i + 1) % 2 === 0) {
+    let chanceToDie = konstantin.smoke('cigarets');
+    if (chanceToDie === Math.floor(Math.random() * 100_000)) {
+      i += Infinity;
+      console.log(`${konstantin.name} умер от сигарет!`);
+    } else {
+      console.log('Живем дальше..');
+    }
+  }
+  if (i % 5 === 0 && i !== 0) {
+    konstantin.want_to_buy(at_krasnoeBeloe, 'vodka');
+  }
+  if ((i + 3) % 5 === 0) {
+    i += 2;
+    konstantin.drink_weekend('vodka');
+  }
+  if (Math.floor(Math.random() * 10_000) === 777) {
+    i += 14;
+    rabota.factoryAccident(konstantin);
+  }
 
-console.log(konstantin.sayHi());
+  if (i % 30 === 0) {
+    rabota.paySalary(konstantin);
+  }
+  if (i % 365 === 0) {
+    konstantin.spendTime();
+    konstantin.growStage();
+  }
+  konstantin.eatFood();
+  console.log(konstantin.sayHi());
+}
