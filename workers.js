693a5707cbcfb28f95d5efec32462c2edd8073a3
JulieV <Trenzelek2009@yandex.ru>
2023-03-12 20:48:50 +0300

    rel

--- workers.js
+++ workers.js
@@ -9,22 +9,89 @@
   3. Покупка имущества, если на него хватает денег (для имущества создай массив объектов)
   4. Смерть работника от старости, либо шанс 0.1% 
 */
+const magazine = [
+  {
+    item: 'Iphone 15',
+    cost: 150_000,
+  },
+  {
+    item: 'BMW x6',
+    cost: 10_000_000,
+  },
+  {
+    item: 'Penthouse',
+    cost: 50_000_000,
+  },
+];
+
 class Work {
   constructor(worker, salary) {
     this.worker = worker;
     this.salary = salary;
   }
+  checkStage(worker) {
+    if (worker.stage >= 5 && worker.stage < 10) {
+      this.salary = 60_000;
+    }
+    if (worker.stage >= 10 && worker.stage < 15) {
+      this.salary = 80_000;
+    }
+    if (worker.stage >= 15) {
+      this.salary = 100_000;
+    }
+  }
 }
 
 class Worker {
-  constructor(name, age, stage, money = 0, items = []) {
+  #doshirak = 200;
+
+  constructor(name, age, stage = 0, money = 0, items = []) {
     this.name = name;
     this.age = age;
     this.stage = stage;
     this.money = money;
     this.items = items;
+    this.isDead = false;
+  }
+  sayHi() {
+return `Hello, my name is ${this.name}, i am ${this.age.toFixed(1)} years old, and i have ${this.money} руб, еще у меня есть ${this.items}`
+} 
+  working(work){
+  if(!this.isDead){
+    for (let i = 0; i < 90; i++) {
+      this.money -+ this.#doshirak
+    }
+    this.money += work.salary
+    this.age += 0.1
+    this.stage += 0.1
+    this.sayHi()
+    if(this.age > 80){
+      this.isDead = Math.random() >= 0.9
+    }
+} else {
+  console.log('я не умер, я в шавасане');
+}
+}
+shopping(magaz){
+ 
+    for (let i = 0; i < magaz.length; i++) {
+      if(this.money >= magaz[i].cost){
+        this.money -= magaz[i].cost
+        this.items.push(magaz.splice(i, 1)[0].item)
+        console.log(`I have spent all my money but now i have ${this.items[this.items.length-1]}`)
+    }
   }
 }
+}
 
-const kostantin = new Worker('Константин', 23, 5);
-const rabota = new Work(kostantin, 30_000);
+const konstantin = new Worker('Константин', 23);
+const rabota = new Work(konstantin, 30_000);
+
+
+for (let i = 0; i < 600; i+=1) {
+  konstantin.working(rabota)
+  rabota.checkStage(konstantin)
+  konstantin.shopping(magazine)
+}
+// console.log();
+console.log(konstantin); 
