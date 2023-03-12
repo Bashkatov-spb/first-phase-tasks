e1d233522acf124631f8303637b65a03a594fb40
Ivan Pushilov <pusilovivan@gmail.com>
2023-03-13 01:06:14 +0300

    workers_done

--- workers.js
+++ workers.js
@@ -14,17 +14,84 @@ class Work {
     this.worker = worker;
     this.salary = salary;
   }
-}
+  checkStage(worker) {
+    if (worker.stage >= 5 && worker.stage < 10) {
+      this.salary = 45_000;
+    }
+    if (worker.stage >= 10 && worker.stage < 15) {
+      this.salary = 65_000;
+    }
+    if (worker.stage >= 15) {
+    this.salary = 85_000;
+    }
+  }
+} 
+const shop = [
+  {
+  item: 'компьютер',
+  cost: 110_000,
+  },
+  {
+    item: 'уаз патриот',
+    cost: 3_500_000,
+    },
+    {
+      item: 'квартира в центре недалеко от работы',
+      cost: 37_500_000,
+      },
+]
 
 class Worker {
+  #food = 400;
   constructor(name, age, stage, money = 0, items = []) {
     this.name = name;
     this.age = age;
     this.stage = stage;
     this.money = money;
     this.items = items;
+    this.isDead = false;
+  }
+
+  sayHi(){
+    console.log(`Привет! Меня зовут ${this.name}, мне ${this.age.toFixed(1)} лет и у меня ${this.money}р а еще у меня есть ${[...this.items]}`) 
+  }
+
+  working(work) {
+    if (!this.isDead) {
+      for (let i = 0; i < 90; i += 1) {
+         this.money -= this.#food;
+      }
+     this.age += 0.1;
+     this.stage += 0.1;
+     this.money += work.salary;
+     this.sayHi();
+     if (this.age > 80) {
+      this.isDead = Math.random() >= 0.9;
+     }
+    }else{
+    console.log ('R.I.P.');
+    }
+  }
+  shopping(shop) {
+    for (let i = 0; i < shop.length; i += 1) {
+      if (this.money >= shop[i].cost) {
+        this.money -= shop[i].cost;
+        const currentItem = shop.splice(i, 1)[0].item;
+        this.items.push(currentItem);
+        console.log(`=================я накопил на ${currentItem}===================`);
+        break;
+      }
+    }
   }
 }
 
-const kostantin = new Worker('Константин', 23, 5);
-const rabota = new Work(kostantin, 30_000);
+const kostantin = new Worker('Константин', 23, 5_000);
+const rabota = new Work('Вкусно_И_Точка', 30_000);
+
+
+for (let i = 0; i < 600; i += 1) {
+  kostantin.working(rabota);
+  rabota.checkStage(kostantin);
+  kostantin.shopping(shop);
+}
+kostantin.sayHi();
